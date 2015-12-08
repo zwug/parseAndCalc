5a6edad6719cf7aa8c0fca5faf3067b059df149b
zwug <zwugme@gmail.com>
2015-12-08 22:23:23 +0300

    initial commit

---
+++ Parser.cs
@@ -0,0 +1,167 @@
+
+using System;
+
+namespace Equatiator {
+
+
+
+public class Parser {
+	public const int _EOF = 0;
+	public const int _number = 1;
+	public const int maxT = 4;
+
+	const bool _T = true;
+	const bool _x = false;
+	const int minErrDist = 2;
+	
+	public Scanner scanner;
+	public Errors  errors;
+
+	public Token t;    // last recognized token
+	public Token la;   // lookahead token
+	int errDist = minErrDist;
+
+
+
+	public Parser(Scanner scanner) {
+		this.scanner = scanner;
+		errors = new Errors();
+	}
+
+	void SynErr (int n) {
+		if (errDist >= minErrDist) errors.SynErr(la.line, la.col, n);
+		errDist = 0;
+	}
+
+	public void SemErr (string msg) {
+		if (errDist >= minErrDist) errors.SemErr(t.line, t.col, msg);
+		errDist = 0;
+	}
+	
+	void Get () {
+		for (;;) {
+			t = la;
+			la = scanner.Scan();
+			if (la.kind <= maxT) { ++errDist; break; }
+
+			la = t;
+		}
+	}
+	
+	void Expect (int n) {
+		if (la.kind==n) Get(); else { SynErr(n); }
+	}
+	
+	bool StartOf (int s) {
+		return set[s, la.kind];
+	}
+	
+	void ExpectWeak (int n, int follow) {
+		if (la.kind == n) Get();
+		else {
+			SynErr(n);
+			while (!StartOf(follow)) Get();
+		}
+	}
+
+
+	bool WeakSeparator(int n, int syFol, int repFol) {
+		int kind = la.kind;
+		if (kind == n) {Get(); return true;}
+		else if (StartOf(repFol)) {return false;}
+		else {
+			SynErr(n);
+			while (!(set[syFol, kind] || set[repFol, kind] || set[0, kind])) {
+				Get();
+				kind = la.kind;
+			}
+			return StartOf(syFol);
+		}
+	}
+
+	
+	void Sample() {
+		int n; 
+		while (la.kind == 2) {
+			Get();
+			Expr(out n);
+			Console.WriteLine(n); 
+		}
+	}
+
+	void Expr(out int n) {
+		int n1; 
+		Term(out n);
+		while (la.kind == 3) {
+			Get();
+			Term(out n1);
+			n = n + n1; 
+		}
+	}
+
+	void Term(out int n) {
+		Expect(1);
+		n = Convert.ToInt32(t.val); 
+	}
+
+
+
+	public void Parse() {
+		la = new Token();
+		la.val = "";		
+		Get();
+		Sample();
+
+	}
+	
+	static readonly bool[,] set = {
+		{_T,_x,_x,_x, _x,_x}
+
+	};
+} // end Parser
+
+
+public class Errors {
+	public int count = 0;                                    // number of errors detected
+	public System.IO.TextWriter errorStream = Console.Out;   // error messages go to this stream
+	public string errMsgFormat = "-- line {0} col {1}: {2}"; // 0=line, 1=column, 2=text
+
+	public virtual void SynErr (int line, int col, int n) {
+		string s;
+		switch (n) {
+			case 0: s = "EOF expected"; break;
+			case 1: s = "number expected"; break;
+			case 2: s = "\"calc\" expected"; break;
+			case 3: s = "\"+\" expected"; break;
+			case 4: s = "??? expected"; break;
+
+			default: s = "error " + n; break;
+		}
+		errorStream.WriteLine(errMsgFormat, line, col, s);
+		count++;
+	}
+
+	public virtual void SemErr (int line, int col, string s) {
+		errorStream.WriteLine(errMsgFormat, line, col, s);
+		count++;
+	}
+	
+	public virtual void SemErr (string s) {
+		errorStream.WriteLine(s);
+		count++;
+	}
+	
+	public virtual void Warning (int line, int col, string s) {
+		errorStream.WriteLine(errMsgFormat, line, col, s);
+	}
+	
+	public virtual void Warning(string s) {
+		errorStream.WriteLine(s);
+	}
+} // Errors
+
+
+public class FatalError: Exception {
+	public FatalError(string m): base(m) {}
+}
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Equatiator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stream inputStream = Console.OpenStandardInput();
+            Console.WriteLine("Write an expression starting with \"calc\" and press enter:");
+            Scanner scanner = new Scanner(inputStream);
+            Parser parser = new Parser(scanner);
+            while(true)
+            {
+                parser.Parse();
+            }
+        }
+    }
+}
